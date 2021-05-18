b4244e161c039a2654e18df97d9886898cdb9a28
xxp <2056709942@qq.com>
2021-05-18 18:56:58 +0800

    ApiGateWay网关调整接口找不到路径问题

--- mall-gateway/api-gateway/src/main/java/com/mallplus/gateway/config/CorsConfigration.java
+++ mall-gateway/api-gateway/src/main/java/com/mallplus/gateway/config/CorsConfigration.java
@@ -14,35 +14,35 @@ import org.springframework.web.filter.CorsFilter;
  * @author mall
  * @date 2018/8/9 15:59
  */
-@Configuration
+//@Configuration
 public class CorsConfigration {
 
-    @Bean
-    public CorsFilter corsFilter() {
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        CorsConfiguration config = new CorsConfiguration();
-        // #允许向该服务器提交请求的URI，*表示全部允许
-        config.addAllowedOrigin(CorsConfiguration.ALL);
-        // 允许cookies跨域
-        config.setAllowCredentials(true);
-        // #允许访问的头信息,*表示全部
-        config.addAllowedHeader(CorsConfiguration.ALL);
-        // 允许提交请求的方法，*表示全部允许
-        config.addAllowedMethod(CorsConfiguration.ALL);
-        source.registerCorsConfiguration("/**", config);
-        return new CorsFilter(source);
-    }
-
-    /**
-     * 配置过滤器
-     */
-    @Bean
-    public FilterRegistrationBean someFilterRegistration() {
-        FilterRegistrationBean<CorsFilter> registration = new FilterRegistrationBean<>();
-        registration.setFilter(corsFilter());
-        registration.addUrlPatterns("/*");
-        registration.setName("corsFilter");
-        registration.setOrder(Ordered.HIGHEST_PRECEDENCE);
-        return registration;
-    }
+//    @Bean
+//    public CorsFilter corsFilter() {
+//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//        CorsConfiguration config = new CorsConfiguration();
+//        // #允许向该服务器提交请求的URI，*表示全部允许
+//        config.addAllowedOrigin(CorsConfiguration.ALL);
+//        // 允许cookies跨域
+//        config.setAllowCredentials(true);
+//        // #允许访问的头信息,*表示全部
+//        config.addAllowedHeader(CorsConfiguration.ALL);
+//        // 允许提交请求的方法，*表示全部允许
+//        config.addAllowedMethod(CorsConfiguration.ALL);
+//        source.registerCorsConfiguration("/**", config);
+//        return new CorsFilter(source);
+//    }
+//
+//    /**
+//     * 配置过滤器
+//     */
+//    @Bean
+//    public FilterRegistrationBean someFilterRegistration() {
+//        FilterRegistrationBean<CorsFilter> registration = new FilterRegistrationBean<>();
+//        registration.setFilter(corsFilter());
+//        registration.addUrlPatterns("/*");
+//        registration.setName("corsFilter");
+//        registration.setOrder(Ordered.HIGHEST_PRECEDENCE);
+//        return registration;
+//    }
 }
