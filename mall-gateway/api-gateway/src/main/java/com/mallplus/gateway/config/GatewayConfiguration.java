3e4e28efe662d664470b2028ea67a94fd04dbd47
“MrXuXiuPing” <2056709942@qq.com>
2021-05-18 23:02:24 +0800

    调整Api网关配置

--- mall-gateway/api-gateway/src/main/java/com/mallplus/gateway/config/GatewayConfiguration.java
+++ mall-gateway/api-gateway/src/main/java/com/mallplus/gateway/config/GatewayConfiguration.java
@@ -1,10 +1,30 @@
 package com.mallplus.gateway.config;
 
+import com.alibaba.csp.sentinel.adapter.gateway.common.rule.GatewayFlowRule;
+import com.alibaba.csp.sentinel.adapter.gateway.common.rule.GatewayRuleManager;
+import com.alibaba.csp.sentinel.adapter.gateway.sc.SentinelGatewayFilter;
+import com.alibaba.csp.sentinel.adapter.gateway.sc.callback.BlockRequestHandler;
+import com.alibaba.csp.sentinel.adapter.gateway.sc.callback.GatewayCallbackManager;
+import com.alibaba.csp.sentinel.adapter.gateway.sc.exception.SentinelGatewayBlockExceptionHandler;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.codec.ServerCodecConfigurer;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import org.springframework.web.reactive.result.view.ViewResolver;
+import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
+
+import javax.annotation.PostConstruct;
+import java.util.*;
 
 /**
  * 路由限流配置
@@ -20,6 +40,14 @@ import reactor.core.publisher.Mono;
  */
 @Configuration
 public class GatewayConfiguration {
+//    private final List<ViewResolver> viewResolvers;
+//    private final ServerCodecConfigurer serverCodecConfigurer;
+//
+//    public GatewayConfiguration(ObjectProvider<List<ViewResolver>> viewResolversProvider,
+//                                ServerCodecConfigurer serverCodecConfigurer) {
+//        this.viewResolvers = viewResolversProvider.getIfAvailable(Collections::emptyList);
+//        this.serverCodecConfigurer = serverCodecConfigurer;
+//    }
 
     @Bean(name="apiKeyResolver")
     @Primary
@@ -45,5 +73,48 @@ public class GatewayConfiguration {
 //    @Bean
 //    KeyResolver userKeyResolver() {
 //        return exchange -> Mono.just(exchange.getRequest().getQueryParams().getFirst("userId"));
+//    }
+
+    //配置限流的异常处理
+//    @Bean
+//    @Order(Ordered.HIGHEST_PRECEDENCE)
+//    public SentinelGatewayBlockExceptionHandler sentinelGatewayBlockExceptionHandler() {
+//        return new SentinelGatewayBlockExceptionHandler(viewResolvers, serverCodecConfigurer);
+//    }
+//
+//    //配置初始化的限流参数
+//    @PostConstruct
+//    public void initGatewayRules(){
+//        Set<GatewayFlowRule> rules = new HashSet<>();
+//        rules.add(
+//                new GatewayFlowRule("provider_route") //路由名称
+//                        .setCount(1)  //数量
+//                        .setIntervalSec(1) //每秒
+//        );
+//        GatewayRuleManager.loadRules(rules);
+//    }
+//
+//    //初始化限流过滤器
+//    @Bean
+//    @Order(Ordered.HIGHEST_PRECEDENCE)
+//    public GlobalFilter sentinelGatewayFilter() {
+//        return new SentinelGatewayFilter();
+//    }
+//
+//    //自定义限流异常页面
+//    @PostConstruct
+//    public void initBlockHandlers(){
+//        BlockRequestHandler blockRequestHandler = new BlockRequestHandler() {
+//            @Override
+//            public Mono<ServerResponse> handleRequest(ServerWebExchange serverWebExchange, Throwable throwable) {
+//                Map map = new HashMap();
+//                map.put("code",0);
+//                map.put("msg","被限流了");
+//                return ServerResponse.status(HttpStatus.OK)
+//                        .contentType(MediaType.APPLICATION_JSON)
+//                        .body(BodyInserters.fromObject(map));
+//            }
+//        };
+//        GatewayCallbackManager.setBlockHandler(blockRequestHandler);
 //    }
 }
