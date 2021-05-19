34ab86be0d63200726fd22fc9980af72943bf944
xxp <2056709942@qq.com>
2021-05-19 19:25:16 +0800

    RabbitMq生产者消费者搭建

---
+++ mall-rabbitmq/rabbitmq-consumer/src/main/java/com/mallplus/consumer/ConsumerApp.java
@@ -0,0 +1,14 @@
+package com.mallplus.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 消费者
+ */
+@SpringBootApplication
+public class ConsumerApp {
+    public static void main(String[] args) {
+        SpringApplication.run(ConsumerApp.class,args);
+    }
+}
---
+++ mall-rabbitmq/rabbitmq-provider/src/main/java/com/mallplus/provider/ProviderApp.java
@@ -0,0 +1,14 @@
+package com.mallplus.provider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 生产者
+ */
+@SpringBootApplication
+public class ProviderApp {
+    public static void main(String[] args) {
+        SpringApplication.run(ProviderApp.class,args);
+    }
+}
