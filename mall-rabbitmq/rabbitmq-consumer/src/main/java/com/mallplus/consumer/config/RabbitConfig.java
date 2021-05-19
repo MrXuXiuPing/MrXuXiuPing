c3a4f32043f6428befeab8f1b55cfacdd99af5f0
“MrXuXiuPing” <2056709942@qq.com>
2021-05-20 00:12:39 +0800

    RabbitMq消费

--- mall-rabbitmq/rabbitmq-consumer/src/main/java/com/mallplus/consumer/config/RabbitConfig.java
+++ mall-rabbitmq/rabbitmq-consumer/src/main/java/com/mallplus/consumer/config/RabbitConfig.java
@@ -5,7 +5,9 @@ import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.DirectExchange;
 import org.springframework.amqp.core.Queue;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class RabbitConfig {
     //队列 起名：TestDirectQueue
     @Bean
